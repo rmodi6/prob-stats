f1e93accd45e4c0fd0630523f34297e352d758e7
Ruchit Modi <modi.ruchit6@gmail.com>
2020-04-28 11:57:50 -0400

    Change log:
    - Added function to compute pdf of normal distribution instead of using library
    - Taking n as length of sample instead of hard coded values
    - Moved computation of se2 inside for loop

--- HW6/q2.py
+++ HW6/q2.py
@@ -1,16 +1,20 @@
 import math
 import numpy as np
-import scipy.stats as stats
 import matplotlib.pyplot as plt
+
+# Compute pdf of normal distribution
+def normal_pdf(x, mu, sigma):
+	return (1 / (np.sqrt(2 * np.pi * np.square(sigma)))) * (np.power(np.e, -(np.square((x - mu)) / (2 * np.square(sigma)))))
+
 
 # Part A
 variance = 9
-n = 100
-se2 = variance / n
 a, b2 = 0, 1
 with open('q2_sigma3.dat') as file:
 	for line in file.readlines():
 		sample = [float(i) for i in line.strip().split(', ')]
+		n = len(sample)
+		se2 = variance / n
 		sample_mean = sum(sample) / n
 		x = ((b2 * sample_mean) + (se2 * a)) / (b2 + se2)
 		y2 = (b2 * se2) / (b2 + se2)
@@ -18,7 +22,7 @@ with open('q2_sigma3.dat') as file:
 		print('{}\t{}'.format(a, b2))
 		sigma = math.sqrt(b2)
 		x_points = np.linspace(a - 3*sigma, a + 3*sigma, 100)
-		plt.plot(x_points, stats.norm.pdf(x_points, a, sigma), label='\u03BC={:.2f} \u03C3={:.2f}'.format(a, sigma))
+		plt.plot(x_points, normal_pdf(x_points, a, sigma), label='\u03BC={:.2f} \u03C3={:.2f}'.format(a, sigma))
 
 plt.title("q2_sigma3.dat")
 plt.xlabel("x")
@@ -29,12 +33,12 @@ plt.show()
 
 # Part B
 variance = 10000
-n = 100
-se2 = variance / n
 a, b2 = 0, 1
 with open('q2_sigma100.dat') as file:
 	for line in file.readlines():
 		sample = [float(i) for i in line.strip().split(', ')]
+		n = len(sample)
+		se2 = variance / n
 		sample_mean = sum(sample) / n
 		x = ((b2 * sample_mean) + (se2 * a)) / (b2 + se2)
 		y2 = (b2 * se2) / (b2 + se2)
@@ -42,7 +46,7 @@ with open('q2_sigma100.dat') as file:
 		print('{}\t{}'.format(a, b2))
 		sigma = math.sqrt(b2)
 		x_points = np.linspace(a - 3*sigma, a + 3*sigma, 100)
-		plt.plot(x_points, stats.norm.pdf(x_points, a, sigma), label='\u03BC={:.2f} \u03C3={:.2f}'.format(a, sigma))
+		plt.plot(x_points, normal_pdf(x_points, a, sigma), label='\u03BC={:.2f} \u03C3={:.2f}'.format(a, sigma))
 
 plt.title("q2_sigma100.dat")
 plt.xlabel("x")
